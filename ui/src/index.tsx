8cb1e71531d1b6374c6d828c383a9520cab59360
bigfood <satcom1600@hanmail.net>
2023-04-14 03:01:25 +0900

    Init tauri

---
+++ ui/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import App from "./component/App";
+
+const container = document.getElementById("app")!;
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
