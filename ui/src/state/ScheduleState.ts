42ceb4cef2bb3c9a4c59389f934ae1b1a392aced
bigfood <satcom1600@hanmail.net>
2023-04-20 16:38:35 +0900

    Save schedules to localstorage

--- ui/src/state/ScheduleState.ts
+++ ui/src/state/ScheduleState.ts
@@ -4,16 +4,46 @@ import { Schedule } from "~../app/bindings/Schedule";
 export namespace ScheduleState {
   export const scheduleIds = atom<string[]>({
     key: "ScheduleState.scheduleIds",
-    default: [],
+    default: JSON.parse(
+      localStorage.getItem("ScheduleState.scheduleIds") || "[]"
+    ),
+    effects: [
+      ({ onSet }) => {
+        onSet((newValue) => {
+          localStorage.setItem(
+            "ScheduleState.scheduleIds",
+            JSON.stringify(newValue)
+          );
+        });
+      },
+    ],
   });
 
   export const schedule = atomFamily<Schedule, string>({
     key: "ScheduleState.schedule",
     default: (id) => {
+      const key = `ScheduleState.schedule-${id}`;
+      const saved = localStorage.getItem(key);
+      if (saved) {
+        return JSON.parse(saved);
+      }
       return {
         id,
         volumePoints: [],
       } as Schedule;
     },
+    effects: [
+      ({ onSet }) => {
+        onSet((newValue, _, isReset) => {
+          const key = `ScheduleState.schedule-${newValue.id}`;
+          console.log(isReset, newValue);
+          if (isReset) {
+            localStorage.removeItem(key);
+          } else {
+            localStorage.setItem(key, JSON.stringify(newValue));
+          }
+        });
+      },
+    ],
   });
 }
